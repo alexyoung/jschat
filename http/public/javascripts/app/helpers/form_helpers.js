8c279ba54686d8efc89a94e1cd932506454e1f77
Alex Young <alex@helicoid.net>
2009-05-26 15:31:33 +0100

    Temporary fix for IE issues with FormHelpers.getCaretPosition

--- http/public/javascripts/app/helpers/form_helpers.js
+++ http/public/javascripts/app/helpers/form_helpers.js
@@ -3,11 +3,21 @@ var FormHelpers = {
     if (element.setSelectionRange) {
       return element.selectionStart;
     } else if (element.createTextRange) {
-      var range = document.selection.createRange();
-      var stored_range = range.duplicate();
-      stored_range.moveToElementText(element);
-      stored_range.setEndPoint('EndToEnd', range);
-      return stored_range.text.length - range.text.length;
+      try {
+        // The current selection
+        var range = document.selection.createRange();
+        // We'll use this as a 'dummy'
+        var stored_range = range.duplicate();
+        // Select all text
+        stored_range.moveToElementText(element);
+        // Now move 'dummy' end point to end point of original range
+        stored_range.setEndPoint('EndToEnd', range);
+
+        return stored_range.text.length - range.text.length;
+      } catch (exception) {
+        // IE is being mental.  TODO: Figure out what IE's issue is
+        return 0;
+      }
     }
   },
 
